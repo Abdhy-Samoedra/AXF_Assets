877751dd47508f9f96736027a38bbfb32188d337
Bella Arsita Ginting <bella20gintingm50@gmail.com>
2024-08-06 22:16:46 +0700

    profile done

--- app/src/main/java/com/example/axf_assets/ProfileActivity.java
+++ app/src/main/java/com/example/axf_assets/ProfileActivity.java
@@ -1,19 +1,31 @@
 package com.example.axf_assets;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.os.Bundle;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    TextView usernameGreeting;
+    TextView usernameHandle;
+    TextView email_user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_profile);
 
+        usernameGreeting = findViewById(R.id.username_greeting);
+        usernameHandle = findViewById(R.id.username_handle);
+
+        // Get the username from the Intent
+        String username = getIntent().getStringExtra("USERNAME");
+
+        // Set the username to the TextViews
+        if (username != null) {
+            usernameGreeting.setText("Hi, " + username);
+            usernameHandle.setText("@" + username);
+//            email_user.setText(username + "@gmail.com");
+        }
     }
 }
